02c0e0b23f6c940ba9f5df68c23da04f1a63c686
zaini5566 <zain7865566@gmail.com>
2025-06-15 15:51:53 +0500

    dropdown

--- src/AddListing/index.jsx
+++ src/AddListing/index.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Header from "../components/Header";
 import carDetails from "../Shared/carDetails.json";
 import InputFields from "./components/inputFields";
-import DropDownField from "./components/DropDownField";
+import DropDownField from "./components/dropDownField";
 import TextAreaField from "./components/TextAreaField";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import { Separator } from "../components/ui/separator";
