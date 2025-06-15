25d0ec9d4cd9f18976551c1a2fd29da0dd697908
zaini5566 <zain7865566@gmail.com>
2025-06-15 14:25:40 +0500

    Hero

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,5 @@
-import { SignInButton } from "@clerk/clerk-react";
-import React from "react";
-
-import { Button } from "./components/ui/button"; // Ensure you have @mui/material installed
 import Header from "./components/Header";
-import Hero from "./components/hero";
+import Hero from "./components/Hero";
 import Category from "./components/Category";
 import MostSearchCar from "./components/MostSearchCar";
 import InfoSection from "./components/InfoSection";
